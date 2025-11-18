84cb0af62bfc8dd9fbed0e2869b7ecc4c4b6dd1d
bhavya-nallamothu <bhavya14051999@gmail.com>
2025-11-18 14:48:10 -0800

    Delete src/main/webapp/js/animation.js

--- src/main/webapp/js/animation.js
+++
@@ -1,6 +0,0 @@
-
-document.getElementById("big-heart").onclick = function(){
-    const m = document.getElementById("missu");
-    m.classList.add("show");
-    setTimeout(()=>m.classList.remove("show"),2500);
-};
